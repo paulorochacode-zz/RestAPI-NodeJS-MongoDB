fc931c0ee8e73fdddcd543a3aa8ff02d3839389e
Paulo <paulorocha7117@gmail.com>
2022-02-28 19:56:18 -0300

    0.04 - EditAndDeleteDone

--- routes/Products.js
+++ routes/Products.js
@@ -1,73 +1,95 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose')
-require ("../models/Products")
-const Product = mongoose.model("Products")
+const mongoose = require('mongoose');
+const { findOne } = require("mongoose/lib/model");
+require("../models/Products");
+const Product = mongoose.model("Products");
 
 // Return all products
 router.get('/', (req, res, next) => {
-      
+
       Product.find().lean().then((products) => {
             res.status(200).send({
-            message: products,
+                  message: products,
             });
       }).catch((err) => {
             res.status(500).send({
                   message: err,
-                  });
-      })
-})
+            });
+      });
+});
 
 //Insert one product
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
 
       const newProduct = new Product({
             name: req.body.name,
             price: req.body.price
-      })
+      });
       newProduct.save().then((Product) => {
             res.status(201).send({
                   message: 'Insert a product',
                   productCreated: newProduct
             });
-      }).catch((err)=>{
+      }).catch((err) => {
             res.status(500).send({
                   message: err,
             });
-      })
+      });
 
 });
 
 //Return data of one product
 router.get('/:id_product', (req, res, next) => {
-      const id = req.params.id_product
-
-      if (id === 'special') {
+      Product.findOne({ _id: req.params.id_product }).lean().then((product) => {
             res.status(200).send({
-                  message: "Special id",
-                  id: id
+                  message: product,
             });
-      } else {
-            res.status(200).send({
-                  message: 'You passed a id',
+      }).catch((err) => {
+            res.status(500).send({
+                  message: err,
             });
-      }
-
-
-})
-
-
-//
-router.patch('/', (req, res, next) => {
-      res.status(201).send({
-            message: 'product altered',
       });
+
 });
 
-router.delete('/', (req, res, next) => {
-      res.status(201).send({
-            message: 'Product deleted',
+
+//to edit a product
+router.patch('/:id_product', (req, res, next) => {
+      var doc = Product.findOne({ _id: req.params.id_product }).then((doc) => {
+            doc.name = req.body.name,
+                  doc.price = req.body.price
+            doc.save().then(() => {
+                  res.status(202).send({
+                        message: 'Altered with success',
+                        ProductAltered: doc,
+                  });
+            }).catch((err) => {
+                  res.status(500).send({
+                        message: 'Error when try to Alter : ' + err,
+                        ProductAltered: doc,
+                  });
+            });
+
       });
-})
+
+});
+
+router.delete('/:id_product', (req, res, next) => {
+      var doc = Product.findOne({ _id: req.params.id_product }).then((doc) => {
+            doc.remove().then(() => {
+                  res.status(202).send({
+                        message: 'Deleted with success',
+                        ProductDeleted: doc,
+                  });
+            }).catch((err) => {
+                  res.status(500).send({
+                        message: 'Error when try to Delete : ' + err,
+                        ProductAltered: doc,
+                  });
+            });
+
+      });
+});
 
 module.exports = router;
